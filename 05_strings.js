4daefc4989355bd1e150cac0a1c78a279ed05548
Abhishek jana <abhishekjana@Abhisheks-MacBook-Pro.local>
2024-12-25 03:06:01 +0530

    updated

---
+++ 05_strings.js
@@ -0,0 +1,10 @@
+// first of all we need to keep in mind that it doesnt really matter what we use
+// like "" or '' javascript will take it as string 
+
+const name = "abishek"
+const repoCount = 2
+console.log(name + " " + repoCount);
+
+//but we should not use this because mordern day production doesnt preffer this 
+
+// instead we will use
