fde9e3e087bf10318feb2fb9100960df10675df0
Abhishek jana <abhishekjana@Abhisheks-MacBook-Pro.local>
2024-12-28 23:24:19 +0530

    brief Intro about JSON

---
+++ basic_API_Intro.js
@@ -0,0 +1,16 @@
+// in mordern days api get the data in the form of JSON. What JSON is .. It is an object kind of a thing without name
+
+// Example of JSON Note we are comminting it because right noe it will give an error
+/*{
+    "name" : "abhishek",
+    "courseName" : "Js hindi",
+    "price" : "free"
+} */
+
+//Sometime we get it as an Array as well
+[
+    {},
+    {},
+    {}
+]
+//Objects inside an array. that time we will use all the functionalities of Array and objects that we have learned previously to handel data
