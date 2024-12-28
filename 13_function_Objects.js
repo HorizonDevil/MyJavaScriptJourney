3c4f105aca9ffeff784b40ea131edf83b8d60f4a
Abhishek jana <abhishekjana@Abhisheks-MacBook-Pro.local>
2024-12-29 02:07:51 +0530

    functions and objects partially done

---
+++ 13_function_Objects.js
@@ -0,0 +1,6 @@
+// suppose we have a situation where number of parameters to the function is not known
+// for example shopping chart where it is how many prodects is going to get added is not known
+function calculateCartprice(...num1){
+return num1
+}
+console.log(calculateCartprice(12,122,122));
