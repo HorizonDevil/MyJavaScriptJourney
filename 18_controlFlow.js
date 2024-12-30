ae0b62b53dcf9ff812003b1d80751d52bcb7e3fd
Abhishek jana <abhishekjana@Abhisheks-MacBook-Pro.local>
2024-12-31 01:02:06 +0530

    control flow partially done

---
+++ 18_controlFlow.js
@@ -0,0 +1,29 @@
+const temp = 20
+
+if(temp == 20){
+    console.log("good");
+
+}
+else {
+    console.log("not good");
+}
+
+const score = 200
+if(score > 100){
+    const power = "fly"
+    console.log(`user power is ${power}`);
+}
+const balance = 1000
+if(balance > 500) console.log("less"),console.log(`more`); //shorthand notation
+
+
+const balance1 = 500
+if(balance1 < 500){
+    console.log(`less`);
+}
+else if(balance1 == 500){
+    console.log(`equal`);
+}
+else{
+    console.log("more");
+}
