3f5a2fc8042364f9debe867d4d10446521ca5990
Abhishek jana <abhishekjana@Abhisheks-MacBook-Pro.local>
2025-01-02 03:08:28 +0530

    partail done

--- 18_controlFlow.js
+++ 18_controlFlow.js
@@ -27,3 +27,59 @@ else if(balance1 == 500){
 else{
     console.log("more");
 }
+
+//multiple condition in if else
+const userLoggedIn = true
+const debitCard = true 
+const loggedInFromGoogle = true
+
+if(userLoggedIn && debitCard && loggedInFromGoogle){  //we need to keep in mind that we can use this "&&" only twice and the condition is both of the the conition must be true
+    console.log("allowed for shopping");
+}
+else{
+    console.log("not allowed for shopping");
+};
+
+{
+    const loggedInFromGoogle = true
+    const loggedInFromEmail = false
+    if(loggedInFromGoogle || loggedInFromEmail){   //OR statement where if any of the condition is true then the code will run
+
+        console.log(`login allowed`);
+    }
+    else{
+        console.log(`not allowed`);
+    }
+}
+
+//*********************Switch Case************************//
+
+/*switch(){
+    case value:
+        break;
+    default:
+        break;
+}*/
+
+//syntex of switch case
+//code to check which month is it
+const month = 3
+switch (month) {
+    case 1:
+        console.log(`january`);
+        break;
+    case 2:
+        console.log(`february`);
+        break;
+    case 3:
+        console.log(`march`);
+        break;
+
+    default:
+        console.log(`please select right month`)
+        break;
+}
+ 
+ 
+
+
